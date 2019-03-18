1008e3c4d9fb83b72949689062bc0fd3bb6bce87
denvyugo <denvyugo@gmail.com>
2019-03-19 00:21:27 +0300

    add habr parse command

--- vb_echo.py
+++ vb_echo.py
@@ -1,6 +1,7 @@
 import telebot
 #from telebot import apihelper
 from telebot.types import Message, User
+import hparse
 
 #apihelper.proxy = {'https':'socks5://telegram:telegram@qcpfo.tgproxy.me:1080'}
 #apihelper.proxy = {'https':'socks5://tg-denvyugo:YpGvFmAi@socksy.seriyps.ru:7777'}
@@ -12,6 +13,12 @@ bot = telebot.TeleBot('758896761:AAGhUuuBz9P-p4lFvP5Dsi3wi4ftc8Jt1NQ')
 def send_welcome(message: Message):
     usr: User = message.from_user
     bot.send_message(chat_id=message.chat.id, text='Hello, {}! It\'s very nice to meet you!'.format(usr.first_name))
+
+
+@bot.message_handler(commands=['habr'])
+def send_welcome(message: Message):
+    text_message = hparse.get_links(hub_name='Python')
+    bot.send_message(chat_id=message.chat.id, text=text_message)
 
 
 @bot.message_handler(func=lambda m: True)
