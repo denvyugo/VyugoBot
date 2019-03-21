ab5a34fbc53058c27dd2e0c46d28d123d43392a7
denvyugo <denvyugo@gmail.com>
2019-03-21 21:33:43 +0300

    bot token to environ

--- vb_echo.py
+++ vb_echo.py
@@ -1,12 +1,10 @@
 import telebot
-#from telebot import apihelper
 from telebot.types import Message, User
 import hparse
+import os
 
-#apihelper.proxy = {'https':'socks5://telegram:telegram@qcpfo.tgproxy.me:1080'}
-#apihelper.proxy = {'https':'socks5://tg-denvyugo:YpGvFmAi@socksy.seriyps.ru:7777'}
-#apihelper.proxy = {'http':'http://10.10.1.10:3128'}
-bot = telebot.TeleBot('758896761:AAGhUuuBz9P-p4lFvP5Dsi3wi4ftc8Jt1NQ')
+TOKEN = os.environ.get('BOT_TOKEN')
+bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=['start', 'help'])
